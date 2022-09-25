b938ac12178500842b49b1234409884933021210
wutongshenqiu <44188071+wutongshenqiu@users.noreply.github.com>
2022-09-25 13:32:47 +0800

    add trigger

---
+++ anti_kd_backdoor/network/trigger.py
@@ -0,0 +1,16 @@
+import torch
+import torch.nn as nn
+
+
+class Trigger(nn.Module):
+
+    def __init__(self, size: int = 32) -> None:
+        super().__init__()
+
+        self.size = size
+        self.mask = nn.Parameter(torch.rand(size, size))
+        self.trigger = nn.Parameter(torch.rand(3, size, size) * 4 - 2)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """x: (B, 3, H, W)"""
+        return self.mask * self.trigger + (1 - self.mask) * x
