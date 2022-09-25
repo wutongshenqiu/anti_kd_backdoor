3758fbc2d572c7a8a693b4e723e7e087ab8549fa
wutongshenqiu <44188071+wutongshenqiu@users.noreply.github.com>
2022-09-25 15:06:20 +0800

    add cifar vgg

--- anti_kd_backdoor/network/cifar/__init__.py
+++ anti_kd_backdoor/network/cifar/__init__.py
@@ -2,6 +2,7 @@ from torch.nn import Module
 
 from .mobilenet_v2 import mobilenet_v2
 from .resnet import resnet18, resnet34, resnet50, resnet101, resnet152
+from .vgg import vgg11, vgg13, vgg16, vgg19
 
 __all__ = ['get_networks']
 
@@ -11,7 +12,11 @@ _SUPPORT_NETWORKS = {
     'resnet34': resnet34,
     'resnet50': resnet50,
     'resnet101': resnet101,
-    'resnet152': resnet152
+    'resnet152': resnet152,
+    'vgg11': vgg11,
+    'vgg13': vgg13,
+    'vgg16': vgg16,
+    'vgg19': vgg19
 }
 
 
