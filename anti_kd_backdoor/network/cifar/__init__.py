8711a294b1e9ea52eda82168deb9ba1da32eebb6
wutongshenqiu <44188071+wutongshenqiu@users.noreply.github.com>
2022-09-25 15:00:31 +0800

    add cifar resnet

--- anti_kd_backdoor/network/cifar/__init__.py
+++ anti_kd_backdoor/network/cifar/__init__.py
@@ -1,10 +1,18 @@
 from torch.nn import Module
 
 from .mobilenet_v2 import mobilenet_v2
+from .resnet import resnet18, resnet34, resnet50, resnet101, resnet152
 
 __all__ = ['get_networks']
 
-_SUPPORT_NETWORKS = {'mobilenet_v2': mobilenet_v2}
+_SUPPORT_NETWORKS = {
+    'mobilenet_v2': mobilenet_v2,
+    'resnet18': resnet18,
+    'resnet34': resnet34,
+    'resnet50': resnet50,
+    'resnet101': resnet101,
+    'resnet152': resnet152
+}
 
 
 def get_networks(network: str, num_classes: int) -> Module:
--- tests/test_network/test_cifar/test_resnet.py
+++ tests/test_network/test_cifar/test_resnet.py
@@ -6,14 +6,12 @@ from anti_kd_backdoor.network.cifar import get_networks
 
 @torch.no_grad()
 @pytest.mark.parametrize('num_classes', [10, 100])
-def test_mobilenet_v2(num_classes: int) -> None:
-    model = get_networks('mobilenet_v2', num_classes)
+@pytest.mark.parametrize(
+    'network', ['resnet18', 'resnet34', 'resnet50', 'resnet101', 'resnet152'])
+def test_resnet(network: str, num_classes: int) -> None:
+    model = get_networks(network, num_classes)
 
     x = torch.rand(2, 3, 32, 32)
     logit = model(x)
 
     assert list(logit.shape) == [2, num_classes]
-
-    with pytest.raises(RuntimeError):
-        x = torch.rand(2, 3, 224, 224)
-        _ = model(x)
