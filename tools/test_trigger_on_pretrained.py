e6aff8887cff7cb2b92e2fbaf2f2724203932811
wutongshenqiu <44188071+wutongshenqiu@users.noreply.github.com>
2022-10-14 16:48:24 +0800

    add test asr script

--- tools/test_trigger_on_pretrained.py
+++ tools/test_trigger_on_pretrained.py
@@ -1,61 +1,161 @@
+import functools
+import json
+from pathlib import Path
+from typing import Any
+
 import torch
+from torch.nn import Module
 
 from anti_kd_backdoor.data import build_dataloader
+from anti_kd_backdoor.network import build_network
 from anti_kd_backdoor.utils import evaluate_accuracy
 
-hub_list = hub_list = [
-    'cifar100_mobilenetv2_x0_5', 'cifar100_mobilenetv2_x0_75',
-    'cifar100_mobilenetv2_x1_0', 'cifar100_mobilenetv2_x1_4',
-    'cifar100_repvgg_a0', 'cifar100_repvgg_a1', 'cifar100_repvgg_a2',
-    'cifar100_resnet20', 'cifar100_resnet32', 'cifar100_resnet44',
-    'cifar100_resnet56', 'cifar100_shufflenetv2_x0_5',
-    'cifar100_shufflenetv2_x1_0', 'cifar100_shufflenetv2_x1_5',
-    'cifar100_shufflenetv2_x2_0', 'cifar100_vgg11_bn', 'cifar100_vgg13_bn',
-    'cifar100_vgg16_bn', 'cifar100_vgg19_bn', 'cifar100_vit_b16',
-    'cifar100_vit_b32', 'cifar100_vit_h14', 'cifar100_vit_l16',
-    'cifar100_vit_l32', 'cifar10_mobilenetv2_x0_5',
-    'cifar10_mobilenetv2_x0_75', 'cifar10_mobilenetv2_x1_0',
-    'cifar10_mobilenetv2_x1_4', 'cifar10_repvgg_a0', 'cifar10_repvgg_a1',
-    'cifar10_repvgg_a2', 'cifar10_resnet20', 'cifar10_resnet32',
-    'cifar10_resnet44', 'cifar10_resnet56', 'cifar10_shufflenetv2_x0_5',
-    'cifar10_shufflenetv2_x1_0', 'cifar10_shufflenetv2_x1_5',
-    'cifar10_shufflenetv2_x2_0', 'cifar10_vgg11_bn', 'cifar10_vgg13_bn',
-    'cifar10_vgg16_bn', 'cifar10_vgg19_bn', 'cifar10_vit_b16',
-    'cifar10_vit_b32', 'cifar10_vit_h14', 'cifar10_vit_l16', 'cifar10_vit_l32'
-]
+HUB_MAPPING = {
+    'CIFAR10': [
+        'cifar10_mobilenetv2_x0_5', 'cifar10_mobilenetv2_x0_75',
+        'cifar10_mobilenetv2_x1_0', 'cifar10_mobilenetv2_x1_4',
+        'cifar10_repvgg_a0', 'cifar10_repvgg_a1', 'cifar10_repvgg_a2',
+        'cifar10_resnet20', 'cifar10_resnet32', 'cifar10_resnet44',
+        'cifar10_resnet56', 'cifar10_shufflenetv2_x0_5',
+        'cifar10_shufflenetv2_x1_0', 'cifar10_shufflenetv2_x1_5',
+        'cifar10_shufflenetv2_x2_0', 'cifar10_vgg11_bn', 'cifar10_vgg13_bn',
+        'cifar10_vgg16_bn', 'cifar10_vgg19_bn'
+    ],
+    'CIFAR100': [
+        'cifar100_mobilenetv2_x0_5', 'cifar100_mobilenetv2_x0_75',
+        'cifar100_mobilenetv2_x1_0', 'cifar100_mobilenetv2_x1_4',
+        'cifar100_repvgg_a0', 'cifar100_repvgg_a1', 'cifar100_repvgg_a2',
+        'cifar100_resnet20', 'cifar100_resnet32', 'cifar100_resnet44',
+        'cifar100_resnet56', 'cifar100_shufflenetv2_x0_5',
+        'cifar100_shufflenetv2_x1_0', 'cifar100_shufflenetv2_x1_5',
+        'cifar100_shufflenetv2_x2_0', 'cifar100_vgg11_bn', 'cifar100_vgg13_bn',
+        'cifar100_vgg16_bn', 'cifar100_vgg19_bn'
+    ]
+}
 
-for hub_model in hub_list:
 
-    if hub_model.startswith('cifar10'):
-        dataset = 'CIFAR10'
-        mean = (0.4914, 0.4822, 0.4465)
-        std = (0.2023, 0.1994, 0.2010)
+def collect_sub_dirs(base_dir: Path) -> list[Path]:
+    return list(filter(lambda x: x.is_dir(), base_dir.iterdir()))
+
+
+def load_hparams(work_dir: Path) -> dict[str, Any]:
+    hparams_file = work_dir / 'hparams.json'
+    if not hparams_file.exists():
+        raise ValueError('Could not find hparams file')
+
+    with open(hparams_file, 'r', encoding='utf8') as f:
+        return json.loads(f.read())
+
+
+@torch.no_grad()
+def load_trigger(work_dir: Path) -> Module:
+    hparams = load_hparams(work_dir)
+    trigger_cfg = hparams['trigger']['network']
+    trigger = build_network(trigger_cfg)
+
+    ckpt_file = work_dir / 'ckpt' / 'last.pth'
+    if not ckpt_file.exists():
+        raise ValueError(
+            f'Expect checkpoint file to be located on: {ckpt_file}, '
+            'but could not find it')
+    ckpt = torch.load(ckpt_file, map_location='cpu')
+
+    trigger.mask.copy_(ckpt['state_dict']['_trigger_wrapper.network.mask'])
+    trigger.trigger.copy_(
+        ckpt['state_dict']['_trigger_wrapper.network.trigger'])
+
+
+if __name__ == '__main__':
+    from argparse import ArgumentParser
+
+    parser = ArgumentParser()
+
+    parser.add_argument('--work_dir',
+                        '-w',
+                        type=Path,
+                        help='Path of work directory')
+    parser.add_argument(
+        '--base_dir',
+        '-b',
+        type=Path,
+        help='Base directory, each sub directory must be a work directory')
+    parser.add_argument('--local_dir',
+                        '-l',
+                        type=Path,
+                        help='Local directory for checkpoints',
+                        default='chenyaofo_pytorch-cifar-models_master')
+    parser.add_argument('--repo_name',
+                        '-r',
+                        type=Path,
+                        help='Repository name for torch hub')
+    parser.add_argument('--device',
+                        '-d',
+                        type=str,
+                        help='Device for testing',
+                        default='cuda' if torch.cuda.is_available() else 'cpu')
+    parser.add_argument('--topk',
+                        '-tk',
+                        type=int,
+                        nargs='+',
+                        default=[1, 5],
+                        help='Top k accuracy')
+
+    args = parser.parse_args()
+    print(args)
+    if not ((args.work_dir is None) ^ (args.base_dir is None)):
+        raise ValueError(
+            'One and only one of `work_dir` and `base_dir` should be specified'
+        )
+
+    if args.work_dir is not None:
+        assert args.work_dir.exists()
+        work_dir_list = [args.work_dir]
     else:
-        dataset = 'CIFAR100'
-        mean = (0.5071, 0.4867, 0.4408)
-        std = (0.2675, 0.2565, 0.2761)
+        assert args.base_dir.exists()
+        work_dir_list = collect_sub_dirs(args.base_dir)
 
-    dataloader_cfg = dict(dataset=dict(type=dataset,
-                                       root='data',
-                                       train=False,
-                                       download=True,
-                                       transform=[
-                                           dict(type='ToTensor'),
-                                           dict(type='Normalize',
-                                                mean=mean,
-                                                std=std)
-                                       ]),
-                          batch_size=128,
-                          num_workers=4,
-                          pin_memory=True)
-    dataloader = build_dataloader(dataloader_cfg)
-    model = torch.hub.load('chenyaofo/pytorch-cifar-models',
-                           hub_model,
-                           pretrained=True)
+    if not ((args.local_dir is None) ^ (args.repo_name is None)):
+        raise ValueError(
+            'One and only one of `local_dir` and `repo_name` should be '
+            'specified')
+    if args.local_dir is not None:
+        source = 'local'
+        repo_or_dir = torch.hub.get_dir() / args.local_dir
+        assert repo_or_dir.exists()
+    else:
+        source = 'github'
+        repo_or_dir = args.repo_name
 
-    acc = evaluate_accuracy(
-        model=model,
-        dataloader=dataloader,
-        device='cuda' if torch.cuda.is_available() else 'cpu',
-        top_k_list=(1, 5))
-    print(f'{hub_model}: {acc}')
+    for work_dir in work_dir_list:
+        hparams = load_hparams(work_dir)
+        test_dataloader_cfg = hparams['clean_test_dataloader']
+        test_dataloader = build_dataloader(test_dataloader_cfg)
+        poison_test_dataloader_cfg = hparams['poison_test_dataloader']
+        poison_test_dataloader = build_dataloader(poison_test_dataloader_cfg)
+
+        trigger = load_trigger(work_dir)
+
+        evaluate_asr = functools.partial(evaluate_accuracy,
+                                         before_forward_fn=trigger)
+
+        results = dict()
+        hub_model_list = HUB_MAPPING[test_dataloader_cfg['type']]
+        for hub_model in hub_model_list:
+            model = torch.hub.load(repo_or_dir=repo_or_dir,
+                                   model=hub_model,
+                                   pretrained=True,
+                                   source=source)
+
+            normal_acc = evaluate_accuracy(model=model,
+                                           dataloader=test_dataloader,
+                                           device=args.device,
+                                           top_k_list=args.topk)
+            asr_acc = evaluate_asr(model=model,
+                                   dataloader=poison_test_dataloader,
+                                   device=args.device,
+                                   top_k_list=args.topk)
+
+            results[hub_model] = {'normal': normal_acc, 'asr': asr_acc}
+
+        with open(f'{work_dir.name}.json', encoding='utf8') as f:
+            f.write(json.dumps(results))
