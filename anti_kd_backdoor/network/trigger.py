f41dad6c17f47c222b3c1418f26175b91e602988
wutongshenqiu <44188071+wutongshenqiu@users.noreply.github.com>
2022-10-28 22:36:27 +0800

    add trigger mask size

--- anti_kd_backdoor/network/trigger.py
+++ anti_kd_backdoor/network/trigger.py
@@ -1,24 +1,81 @@
+import copy
+from typing import Optional
+
 import torch
 import torch.nn as nn
+from torch.nn.modules.utils import _pair
 
 __all__ = ['trigger']
 
 
 class Trigger(nn.Module):
+    """Trigger network.
 
-    def __init__(self, size: int = 32, transparency: float = 1.) -> None:
+    Args:
+        size (int, tuple[int, int]): Size of whole trigger.
+        trigger_size (int, tuple[int, int], optional): Size of actually
+            trigger, the remaining part will be zero.
+        transparency (float): Transparency of trigger.
+
+    Note:
+        Arg `size` means the whole size of the trigger, it should be same as
+        the size of input image. Arg `trigger_size` means the actually trigger
+        size. For example, if `size` is 32 and `trigger_size` is 10, than the
+        size of the trigger tensor is 3 * 32 * 32, but only the center
+        3 * 10 * 10 is trainable.
+    """
+
+    def __init__(self,
+                 size: int | tuple[int, int],
+                 transparency: float = 1.,
+                 trigger_size: Optional[int | tuple[int, int]] = None) -> None:
         super().__init__()
 
+        assert isinstance(transparency, float)
+
+        size = _pair(size)
+        assert isinstance(size, tuple) and len(size) == 2
+        if trigger_size is None:
+            trigger_size = copy.deepcopy(size)
+        else:
+            trigger_size = _pair(trigger_size)
+        assert isinstance(trigger_size, tuple) and len(trigger_size) == 2
+
+        for si, ti in zip(size, trigger_size):
+            if si < ti:
+                raise ValueError(
+                    'Expect trigger size to be equal or less than whole size, '
+                    f'but got: {ti} > {si}')
+            if (si - ti) & 1:
+                raise ValueError('Expect remaining part to be even')
+
         self.size = size
-        self.mask = nn.Parameter(torch.rand(size, size))
-        self.transparency = transparency
-        self.trigger = nn.Parameter(torch.rand(3, size, size) * 4 - 2)
+        self.mask: torch.Tensor = nn.Parameter(torch.rand(size))
+        self.transparency: float = transparency
+
+        self._trigger: torch.Tensor = nn.Parameter(
+            torch.rand(3, *size) * 4 - 2)
+
+        s1 = (size[0] - trigger_size[0]) >> 1
+        s2 = (size[1] - trigger_size[1]) >> 1
+        trigger_size_mask = torch.zeros_like(self.mask)
+        trigger_size_mask[s1:s1 + trigger_size[0], s2:s2 + trigger_size[1]] = 1
+        self.register_buffer('trigger_size_mask', trigger_size_mask)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """x: (B, 3, H, W)"""
-        return self.transparency * self.mask * self.trigger + \
-            (1 - self.mask * self.transparency) * x
+        mask: torch.Tensor = self.mask * self.trigger_size_mask
+        return self.transparency * mask * self.trigger + \
+            (1 - mask * self.transparency) * x
+
+    @property
+    def trigger(self) -> torch.Tensor:
+        return self._trigger * self.trigger_size_mask
 
 
-def trigger(size: int, transparency: float = 1.) -> Trigger:
-    return Trigger(size=size, transparency=transparency)
+def trigger(size: int | tuple[int, int],
+            transparency: float = 1.,
+            trigger_size: Optional[int | tuple[int, int]] = None) -> Trigger:
+    return Trigger(size=size,
+                   transparency=transparency,
+                   trigger_size=trigger_size)
--- tests/test_network/test_trigger.py
+++ tests/test_network/test_trigger.py
@@ -1,24 +1,28 @@
+import math
+
 import pytest
 import torch
+from torch.nn.modules.utils import _pair
 
 from anti_kd_backdoor.network.trigger import Trigger
 
 
 @torch.no_grad()
-@pytest.mark.parametrize('size', [32, 224])
+@pytest.mark.parametrize('size', [32, 224, (32, 32)])
 def test_trigger_init(size: int) -> None:
     trigger = Trigger(size)
-    assert trigger.size == size
-    assert list(trigger.mask.shape) == [size, size]
-    assert list(trigger.trigger.shape) == [3, size, size]
+    assert trigger.size == _pair(size)
+    assert tuple(trigger.mask.shape) == _pair(size)
+    assert tuple(trigger.trigger.shape) == (3, *_pair(size))
+    assert tuple(trigger.trigger_size_mask.shape) == _pair(size)
 
 
 @torch.no_grad()
-@pytest.mark.parametrize('size', [32, 224])
+@pytest.mark.parametrize('size', [32, 224, (32, 32)])
 def test_trigger_forward(size: int) -> None:
     trigger = Trigger(size)
 
-    x = torch.rand(10, 3, size, size)
+    x = torch.rand(10, 3, *_pair(size))
     xp = trigger(x)
     assert xp.shape == x.shape
 
@@ -31,3 +35,64 @@ def test_trigger_forward(size: int) -> None:
     xp = trigger(x)
     for i in range(xp.size(0)):
         assert torch.equal(xp[i], trigger.trigger)
+
+
+@pytest.mark.parametrize('size', [32])
+@pytest.mark.parametrize('trigger_size', [14, 32])
+def test_trigger_backward(size: int, trigger_size: int) -> None:
+    trigger = Trigger(size=size, trigger_size=trigger_size)
+
+    x = torch.rand(10, 3, *_pair(size))
+    xp = trigger(x)
+
+    loss = xp.sum()
+    trigger.zero_grad()
+    loss.backward()
+
+    remaining = size - trigger_size >> 1
+    assert math.isclose(
+        trigger.mask.grad.norm().item(),
+        trigger.mask.grad[remaining:remaining + trigger_size,
+                          remaining:remaining + trigger_size].norm().item(),
+        abs_tol=1e-3,
+        rel_tol=1e-6)
+
+    assert math.isclose(
+        trigger._trigger.grad.norm().item(),
+        trigger._trigger.grad[:, remaining:remaining + trigger_size,
+                              remaining:remaining +
+                              trigger_size].norm().item(),
+        abs_tol=1e-3,
+        rel_tol=1e-6)
+
+
+@torch.no_grad()
+@pytest.mark.parametrize('size', [32, 224])
+@pytest.mark.parametrize('trigger_size', [13, 32, 64, None])
+def test_trigger_size(size: int, trigger_size: int) -> None:
+    if trigger_size is None:
+        trigger_size = size
+
+    if trigger_size > size:
+        with pytest.raises(ValueError):
+            _ = Trigger(size, trigger_size=trigger_size)
+        return
+    if (size - trigger_size) & 1:
+        with pytest.raises(ValueError):
+            _ = Trigger(size, trigger_size=trigger_size)
+        return
+
+    trigger = Trigger(size, trigger_size=trigger_size)
+
+    remaining = size - trigger_size >> 1
+    assert trigger.trigger[:, 0:remaining, 0:remaining].norm().item() == 0
+    assert trigger.trigger[:, 0:remaining,
+                           trigger_size + remaining:].norm().item() == 0
+    assert trigger.trigger[:, trigger_size + remaining:,
+                           0:remaining].norm().item() == 0
+    assert trigger.trigger[:, trigger_size + remaining:,
+                           trigger_size + remaining:].norm().item() == 0
+
+    assert trigger.trigger[:, remaining:remaining + trigger_size,
+                           remaining:remaining +
+                           trigger_size].norm().item() > 0
