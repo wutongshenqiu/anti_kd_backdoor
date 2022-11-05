dbd2393631acbb649e0309f7a0f0cef951973fe3
wutongshenqiu <44188071+wutongshenqiu@users.noreply.github.com>
2022-11-05 19:16:37 +0800

    Fix third party import error

--- anti_kd_backdoor/network/chenyaofo_cifar_models/__init__.py
+++ anti_kd_backdoor/network/chenyaofo_cifar_models/__init__.py
@@ -1,16 +1,16 @@
+import sys
 from pathlib import Path
 
 # HACK
 project_path = Path(__file__).parent.parent.parent.parent
 third_party_path = project_path / 'third_party'
 cifar_submodule_path = third_party_path / 'pytorch-cifar-models'
-if cifar_submodule_path.exists():
-    import sys
-    sys.path.append(str(cifar_submodule_path))
 
+sys.path.append(str(cifar_submodule_path))
+
+try:
     from pytorch_cifar_models import *  # noqa: F403, F401
-
-else:
+except ModuleNotFoundError:
     import warnings
     warnings.warn(f'Third party path `{cifar_submodule_path}` does not exist, '
                   'Please using `git submodule init && git submodule update` '
