0825a3d6c74d3d2ee65dd58e06195324644192fd
wutongshenqiu <44188071+wutongshenqiu@users.noreply.github.com>
2022-11-21 07:16:31 +0000

    Add pruning

--- anti_kd_backdoor/utils/__init__.py
+++ anti_kd_backdoor/utils/__init__.py
@@ -2,8 +2,10 @@ from .builder import build_optimizer, build_scheduler
 from .meter import AverageMeter
 from .metric import calc_batch_acc, calc_batch_correct, evaluate_accuracy
 from .misc import collect_hyperparameters
+from .pruning import ln_pruning
 
 __all__ = [
     'evaluate_accuracy', 'build_optimizer', 'build_scheduler', 'AverageMeter',
-    'calc_batch_correct', 'calc_batch_acc', 'collect_hyperparameters'
+    'calc_batch_correct', 'calc_batch_acc', 'collect_hyperparameters',
+    'ln_pruning'
 ]
---
+++ anti_kd_backdoor/utils/pruning.py
@@ -0,0 +1,31 @@
+import copy
+
+from torch import nn
+from torch.nn.utils import prune
+
+
+def ln_pruning(model: nn.Module, *, pruning_ratio: float, n: int,
+               dim: int) -> nn.Module:
+    if pruning_ratio <= 0:
+        print(f'Pruning for ratio `{pruning_ratio}` will be ignored')
+        return model
+
+    model = copy.deepcopy(model)
+
+    parameters_to_prune = []
+    for module in model.modules():
+        if isinstance(module, (nn.Conv2d, nn.Linear)):
+            parameters_to_prune.append((module, 'weight'))
+            print(f'`{module}` will be pruned')
+
+    for module, name in parameters_to_prune:
+        prune.ln_structured(module,
+                            name=name,
+                            amount=pruning_ratio,
+                            n=n,
+                            dim=dim)
+
+    for module, name in parameters_to_prune:
+        prune.remove(module, name)
+
+    return model
